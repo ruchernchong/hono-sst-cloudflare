286d54b33d00a636897998426e89ea46c8838d6b
Ru Chern Chong <hello@ruchern.xyz>
2024-04-13 22:29:54 +0800

    Use typicode posts data for demo purposes

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,26 @@
 import { Hono } from "hono";
-import { Resource } from "sst";
 
 const app = new Hono();
 
 app.get("/", async (c) => {
-  const first = await Resource.HonoSSTBucket.list().then(
-    (res) =>
-      res.objects.sort(
-        (a, b) => a.uploaded.getTime() - b.uploaded.getTime(),
-      )[0],
-  );
-  const result = await Resource.HonoSSTBucket.get(first.key);
-  c.header("content-type", result.httpMetadata.contentType);
-  return c.body(result.body);
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => res)
+    .then((res) => res.json())
+    .catch((error) => console.error(error));
+
+  return c.body(JSON.stringify(response));
 });
 
-app.put("/*", async (c) => {
-  const key = crypto.randomUUID();
-  await Resource.HonoSSTBucket.put(key, c.req.raw.body, {
-    httpMetadata: {
-      contentType: c.req.header("content-type"),
-    },
-  });
-  return new Response(`Object created with key: ${key}`);
-});
-
-app.get("/posts/:id", (c) => {
-  const page = c.req.query("page");
+app.get("/posts/:id", async (c) => {
   const id = c.req.param("id");
-  c.header("X-Message", "Hi!");
-  return c.text(`You want see ${page} of ${id}`);
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${id}`,
+  )
+    .then((res) => res)
+    .then((res) => res.json())
+    .catch((error) => console.error(error));
+
+  return c.body(JSON.stringify(response));
 });
-
-app.post("/posts", (c) => c.text("Created!", 201));
-
-app.put("/posts/:id", async (c) => {
-  const key = crypto.randomUUID();
-  await Resource.HonoSSTBucket.put(key, c.req.raw.body, {
-    httpMetadata: {
-      contentType: c.req.header("content-type"),
-    },
-  });
-  return new Response(`Object created with key: ${key}`);
-});
-
-app.delete("/posts/:id", (c) => c.text(`${c.req.param("id")} is deleted!`));
 
 export default app;
