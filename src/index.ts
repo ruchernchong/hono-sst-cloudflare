fd3ca0abb0f31ba5242e9f12d1f6020b26e511ce
Ru Chern Chong <hello@ruchern.xyz>
2024-04-13 14:06:06 +0800

    Add additional API endpointApps

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,15 @@ app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
+app.get("/posts/:id", (c) => {
+  const page = c.req.query("page");
+  const id = c.req.param("id");
+  c.header("X-Message", "Hi!");
+  return c.text(`You want see ${page} of ${id}`);
+});
+
+app.post("/posts", (c) => c.text("Created!", 201));
+
+app.delete("/posts/:id", (c) => c.text(`${c.req.param("id")} is deleted!`));
+
 export default app;
