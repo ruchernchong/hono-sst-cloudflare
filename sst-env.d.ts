159375d908e651f934bdc20293dd1130da2fb5b9
Ru Chern Chong <hello@ruchern.xyz>
2024-06-20 23:26:13 +0800

    Add SST types

---
+++ sst-env.d.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+import "sst"
+declare module "sst" {
+  export interface Resource {
+    Hono: {
+      type: "sst.cloudflare.Worker"
+      url: string
+    }
+  }
+}
